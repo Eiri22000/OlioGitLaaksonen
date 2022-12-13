90092afbce8d453acd76b11f1e6e9322cbf53d3d
Eiri22000 <113633681+Eiri22000@users.noreply.github.com>
2022-12-13 18:08:01 +0200

    Create code
    Create code start

---
+++ src/LeluLuokka.java
@@ -0,0 +1,29 @@
+
+public class LeluLuokka {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}//End main
+
+class Lelu
+{
+	String nimi;
+	public double paino;
+
+public Lelu()
+{
+	nimi = "auto";
+	paino = 0.2;
+	}
+
+public Lelu(String nimi, double paino)
+{
+	this.nimi = nimi;
+	this.paino = paino;
+}
+
+}
+
